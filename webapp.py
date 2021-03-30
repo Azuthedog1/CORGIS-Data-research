eaba4f773c366849954624e870ea73d69b835605
Azuthedog1 <77691109+Azuthedog1@users.noreply.github.com>
2021-03-30 10:13:49 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+	with open('earthquakes.json') as earthquake_data:
+		locations = json.load(earthquake_data)
+	return render_template('home.html', options = get_location_options(locations))
+
+@app.route("/response")
+def render_response():
+	with open('earthquakes.json') as earthquake_data:
+		locations = json.load(earthquake_data)
+	location = request.args['LocationSelected']
+	fact1 = 0
+	#for data in location:
+		#if place == data["location"]["name"]:
+			#factC = data["Miscellaneous"]["Language Other than English at Home"]
+			#factS = data["State"]
+	return render_template('response.html', response = fact1)
+
+
+if __name__=="__main__":
+    app.run(debug=True, port=54321)
